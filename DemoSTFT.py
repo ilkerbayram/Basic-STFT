957fcce2b12b7c9fc0acd50fc35958428ed7dd57
Ilker Bayram <ibayram@ieee.org>
2019-06-13 09:42:19 -0400

    Add files via upload

--- DemoSTFT.py
+++ DemoSTFT.py
@@ -6,9 +6,8 @@ Created on Thu Oct 20 13:45:04 2016
 """
 # demonstrates the use of the routines in the STFT package
 
-import numpy
+import numpy as np
 import STFT
-#import pdb
 import pylab
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
@@ -19,36 +18,38 @@ x = pylab.double(x) / 2.**15 # convert integer to double
 
 # STFT parameters
 winms = 60 # length of the window in milliseconds
-win = numpy.hamming(winms*fs/1000)  # we use a Hamming window but will modify it 
+win = np.hamming(int(winms*fs/1000))  # we use a Hamming window but will modify it
 
 hopms = 15 # hop size in milliseconds
-hop = hopms*fs/1000 # hop size in samples
+hop = int(hopms*fs/1000) # hop size in samples
 
 win2 = STFT.NormalizeWindow(win,hop) # this normalization step ensures that the STFT is self-inverting (or a Parseval frame)
 
 # Compute the STFT
+# zero pad to ensure that there are no partial overlap windows in the STFT computation
+x = np.pad(x, (win.size + hop, win.size + hop), 'constant', constant_values=(0, 0))
 X = STFT.STFT(x,win2,hop)
 
 # Display the STFT
-Fr = numpy.array([0,3000]) # Frequency range to display
-clim = [-10, 0] # magnitude range in dB
+Fr = np.array([0,3000]) # Frequency range to display
+clim = np.array([-10, 0]) # magnitude range in dB
 
-Norm = numpy.amax(abs(X))  
+Norm = np.amax(abs(X))  
 X2 = X / Norm # normalize so that the largest STFT coefficient magnitude is unity
-
+fig, ax = plt.subplots(1,1,figsize = (10,5))
 STFT.DisplaySTFT(X2,fs,hop,Fr,clim) # main display function for the STFT
 
 # Inverse STFT
 y = STFT.ISTFT(X,win2,hop)
-yr = numpy.real(y)
+yr = np.real(y)
 
 # check that ISTFT actually inverts the STFT
-t = numpy.arange(0,yr.size)
-t = pylab.double(t) / pylab.double(fs)
+t = np.arange(0,x.size) / fs 
 
 # plot the original and the difference of the original from the reconstruction
-plt.figure()
-plt.plot(t,x[0:yr.size],'b-',label="Original")
-plt.plot(t,x[0:yr.size]-yr,'r-',label = "Difference of the Reconstruction and the Original")
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, mode="expand", borderaxespad=0.)
-plt.xlabel('Time (sec)')
+fig, ax = plt.subplots(1,1,figsize = (10,5))
+ax.plot(t,x,'b-',label="Original")
+ax.plot(t,x-yr[:x.size],'r-',label = "Difference of the Reconstruction and the Original")
+ax.set_xlabel('Time (sec)')
+ax.set_title('Maximum Absolutte Reconstruction Error : {:.4e}'.format(np.max(np.abs(x-yr[:x.size]))))
+ax.legend()
