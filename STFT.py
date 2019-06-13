7b59de1731d2cdea0c93631e34f147a069c51078
Ilker Bayram <ibayram@ieee.org>
2019-06-13 09:42:47 -0400

    Add files via upload

--- STFT.py
+++ STFT.py
@@ -5,8 +5,7 @@ Created on Tue Oct 11 15:36:09 2016
 
 @author: ilker bayram
 """
-import numpy
-import scipy
+import numpy as np
 import matplotlib.pyplot as plt
 
 def STFT(x,win,hop):
@@ -17,17 +16,18 @@ def STFT(x,win,hop):
     #
     # output variables : 
     # X : the STFT coefficients of x
-    L = len(x) # length of the audio signal
-    N = len(win) #length of the window -- fft size 
-    K = (L-N)/hop + 1 # number of steps to take
-    X = numpy.zeros((N,K), dtype = complex ) # will hold the STFT coefficients
+    Lx = x.size # length of the audio signal
+    Lwin = win.size #length of the window -- fft size 
+    Kstep = (np.ceil((Lx-Lwin)/hop) + 1).astype(int)# number of steps to take
+    X = np.zeros((Lwin,Kstep), dtype = complex ) # will hold the STFT coefficients
     k = 0 # time index
-    nf = numpy.sqrt(N) # normalizing factor
-    while (k < K):
-        d = x[ k * hop : k * hop + N ] * win
-        D = scipy.fft(d)
-        X[:,k] = D / nf
-        k += 1
+    nf = np.sqrt(Lwin) # normalizing factor
+    for k in range(Kstep-1):
+        d = x[ k * hop : k * hop + Lwin ] * win
+        X[:,k] = np.fft.fft(d) / nf
+    # the last window may partially overlap with the signal
+    d = x[ Kstep * hop : ]
+    X[:,k] = np.fft.fft(d * win[:d.size], n = Lwin) / nf
     return X
     
 def ISTFT(X,win,hop):
@@ -38,24 +38,24 @@ def ISTFT(X,win,hop):
     #
     # output variables : 
     # x : inverse STFT of X
-    N = X.shape[0] # length of the window
-    K = X.shape[1] # number of frames
-    L = N + (K-1) * hop # length of the output signal
-    x = numpy.zeros((L), dtype = complex)
+    Lwin = X.shape[0] # length of the window
+    Kstep = X.shape[1] # number of frames
+    Lx = Lwin + (Kstep - 1) * hop # length of the output signal
+    x = np.zeros((Lx), dtype = complex)
     k = 0
-    fac = numpy.sqrt(N) # normalizing factor
-    while (k < K):
-        d = scipy.ifft(X[:,k])
+    fac = np.sqrt(Lwin) # normalizing factor
+    for k in range(Kstep):
+        d = np.fft.ifft(X[:,k])
         d = fac * d * win
-        x[k*hop:k*hop+N] += d
-        k += 1
+        x[ k * hop : k * hop + Lwin] += d
+
     return x
     
 def NormalizeWindow(win,hop):
     # normalize the window according to the 
     # provided hop-size so that the STFT is a tight frame
-    N = len(win)
-    K = N / hop
+    N = win.size
+    K = int(N / hop)
     win2 = win * win
     z = 1 * win2
     k = 1
@@ -67,7 +67,7 @@ def NormalizeWindow(win,hop):
         ind1 -= hop
         ind2 += hop
         k += 1
-    win2 = win / numpy.sqrt(z)
+    win2 = win / np.sqrt(z)
     return win2
     
 def DisplaySTFT(X,fs,hop,Fr,clim):
@@ -78,9 +78,9 @@ def DisplaySTFT(X,fs,hop,Fr,clim):
     # Fr : frequency range (in Hz) to display (length two array)
     # clim : illumination range
     N = X.shape[0]
-    Fd = Fr * N / fs
+    Fd = (Fr * N / fs).astype(int)
     Z = X[Fd[1]:Fd[0]:-1,:]
-    Z = numpy.clip(numpy.log(numpy.abs(Z)),clim[0],clim[1])
+    Z = np.clip(np.log(np.abs(Z) + 1e-50),clim[0],clim[1])
     Z = 255 * (Z - clim[0]) / (clim[1] - clim[0])
     time = float(hop) / float(fs) * float(X.shape[1])
     plt.imshow(Z,extent=[0,time,Fr[0] / 1000,Fr[1] / 1000],aspect = "auto") 
